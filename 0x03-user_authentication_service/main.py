2aab1f90e74d329ff0937584cd70945ab2f85350
Amkeez252 <auwalamalik@gmail.com>
2023-08-17 15:41:40 +0000

    User authentication service

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,10 +1,56 @@
-#!/usr/bin/env python3
-"""
-Main file
-"""
-from user import User
+import requests
 
-print(User.__tablename__)
+BASE_URL = "http://your-web-server-url"  # Replace with your actual web server URL
 
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
+def register_user(email: str, password: str) -> None:
+    url = f"{BASE_URL}/register"
+    data = {"email": email, "password": password}
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    url = f"{BASE_URL}/login"
+    data = {"email": email, "password": password}
+    response = requests.post(url, json=data)
+    assert response.status_code == 401
+
+def log_in(email: str, password: str) -> str:
+    url = f"{BASE_URL}/login"
+    data = {"email": email, "password": password}
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+    return response.json()["session_id"]
+
+def profile_unlogged() -> None:
+    url = f"{BASE_URL}/profile"
+    response = requests.get(url)
+    assert response.status_code == 401
+
+def profile_logged(session_id: str) -> None:
+    url = f"{BASE_URL}/profile"
+    headers = {"Authorization": f"Bearer {session_id}"}
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 200
+
+def log_out(session_id: str) -> None:
+    url = f"{BASE_URL}/logout"
+    headers = {"Authorization": f"Bearer {session_id}"}
+    response = requests.post(url, headers=headers)
+    assert response.status_code == 200
+
+def reset_password_token(email: str) -> str:
+    url = f"{BASE_URL}/reset_password"
+    data = {"email": email}
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+    return response.json()["reset_token"]
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    url = f"{BASE_URL}/update_password"
+    data = {"email": email, "reset_token": reset_token, "new_password": new_password}
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+
+if __name__ == "__main__":
+    # You can call and test your functions here
+    pass
