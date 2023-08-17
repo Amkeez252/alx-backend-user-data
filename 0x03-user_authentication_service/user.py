9fd8f0edc1053944a7192b79b7b5a61849951456
Amkeez252 <auwalamalik@gmail.com>
2023-08-17 15:29:09 +0000

    User authentication service

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" User model """
-
-from sqlalchemy import create_engine, Column, Integer, String
+""" User model
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
 
 class User(Base):
     """ User class """
